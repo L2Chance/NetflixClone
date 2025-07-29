db5485ef846b76e1ae298dc75c4d63ee893bf60f
L2Chance <Estudiandoyavanzando@gmail.com>
2025-07-28 22:26:25 -0300

    Mejorado el diseño de las cards desplegables de HOME para ser más coincidentes con las originales de Netflix

---
+++ src/components/BotonCirculo.tsx
@@ -0,0 +1,19 @@
+import { FaPlay, FaPlus, FaThumbsUp } from "react-icons/fa";
+
+export default function BotonCirculo() {
+  return (
+    <div className="flex gap-2 items-center p-2">
+      <button className="w-10 h-10 sm:w-12 sm:h-12 rounded-full bg-white flex items-center justify-center">
+        <FaPlay className="text-black text-sm sm:text-base" />
+      </button>
+
+      <button className="w-10 h-10 sm:w-12 sm:h-12 rounded-full border-2 border-white flex items-center justify-center text-white hover:bg-white/20">
+        <FaPlus className="text-sm sm:text-base" />
+      </button>
+
+      <button className="w-10 h-10 sm:w-12 sm:h-12 rounded-full border-2 border-white flex items-center justify-center text-white hover:bg-white/20">
+        <FaThumbsUp className="text-sm sm:text-base" />
+      </button>
+    </div>
+  );
+}
