3c5a3fa117619357df71a16be1acc931b15d7eba
L2Chance <Estudiandoyavanzando@gmail.com>
2025-08-04 14:49:29 -0300

    Agregado estados de carga más vistosos y función para crear peliculas

--- src/pages/PeliculaDetalle.tsx
+++ src/pages/PeliculaDetalle.tsx
@@ -49,8 +49,31 @@ export default function PeliculaDetalle() {
 
   if (isLoading) {
     return (
-      <div className="p-4 text-center text-white mt-20">
-        Cargando película...
+      <div className="flex flex-col items-center justify-center h-200  text-white">
+        <svg
+          className="animate-spin h-12 w-12 text-red-600 mb-4"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          aria-label="Loading spinner"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+          ></path>
+        </svg>
+        <p className="text-xl font-semibold tracking-wide">
+          Cargando<span className="animate-pulse">...</span>
+        </p>
       </div>
     );
   }
